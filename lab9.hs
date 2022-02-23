b00a8d859d8dec8638d2d750d63d538d5af04cc0
James H. Schader <schadejh@dukes.jmu.edu>
2022-02-23 12:11:20 -0500

    trying to fix divBy15

--- lab9.hs
+++ lab9.hs
@@ -4,8 +4,8 @@
 sumOfFirstN :: Integral a => a -> a
 sumOfFirstN n = n * (n+1) `div` 2
 
-divBy15 :: Integral x => Double -> Bool
-divBy15 x = (0 == (x `mod` 15))
+--divBy15 :: Num x => Double -> Bool
+--divBy15 x = (0 == (x `mod` 15))
 
 points :: Integral a => a -> a
 points n
