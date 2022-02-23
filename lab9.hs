3fbde93f911254047da796db245d05e97ad0b677
James H. Schader <schadejh@dukes.jmu.edu>
2022-02-23 12:18:31 -0500

    got divBy15 working

--- lab9.hs
+++ lab9.hs
@@ -4,14 +4,16 @@
 sumOfFirstN :: Integral a => a -> a
 sumOfFirstN n = n * (n+1) `div` 2
 
---divBy15 :: Num x => Double -> Bool
---divBy15 x = (0 == (x `mod` 15))
+divBy15 :: Integral a => a -> Bool
+divBy15 x = (0 == (x `mod` 15))
 
 points :: Integral a => a -> a
 points n
   | (9 < n) || (n < 1) = 0
   | (n <= 3)           = 12 - 2*n
   | otherwise          = 9 - n
+
+--nextOdd :: Num n => n -> Integer
 
 sumList :: Num p => [p] -> p
 sumList [] = 0
